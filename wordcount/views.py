ae6fc8b281718ec0572cee3d135e103f18fd4d8d
Leonardo Custodio Dorneles <lcdorneles@timbrasil.com.br>
2020-07-24 14:01:56 -0300

    feature - no case sensitive

--- wordcount/views.py
+++ wordcount/views.py
@@ -7,7 +7,19 @@ def home(request):
 def result(request):
 
     fulltext = request.GET['fulltext']
-    count = len(fulltext.split())
+    wordlist = fulltext.split()
+
+    temp = []
+    count = 0
+
+    for word in wordlist:
+
+        if word.lower() not in temp:
+
+            count +=1
+            temp.append(word.lower())
+
+    #count = len(fulltext.split())
 
     return render(request, 'result.html', {'fulltext': fulltext, 'count': count})
 
